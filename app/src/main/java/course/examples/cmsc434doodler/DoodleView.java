b2ef780ef8dc42d04e6e2e640a2befd72d7613d9
Nick Valdez <jnicolasvaldez@gmail.com>
2016-03-20 21:12:39 -0400

    Completed Backend

--- app/src/main/java/course/examples/cmsc434doodler/DoodleView.java
+++ app/src/main/java/course/examples/cmsc434doodler/DoodleView.java
@@ -9,6 +9,11 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Created by Nicolas on 3/9/2016.
  */
@@ -17,6 +22,12 @@ public class DoodleView extends View {
     private Paint mPaintDoodle = new Paint();
     /*Array of points*/
     private Path mPath = new Path();
+    private List<Path> mPaths;
+    private List<Paint> mPaintObjects;
+    private Float mStrokeWidth;
+    private int mOpacity;
+    private int mColor;
+
 
     public DoodleView(Context context) {
         super(context);
@@ -34,17 +45,30 @@ public class DoodleView extends View {
     }
 
     private void init(AttributeSet atts, int defStyle) {
-        mPaintDoodle.setColor(Color.RED);
+        mOpacity = 255;
+        mStrokeWidth = 1.0F;
+        mColor = Color.RED;
+        setPaintProperties();
+        mPaths = new ArrayList<>();
+        mPaintObjects = new ArrayList<>();
+
+    }
+
+    private void setPaintProperties() {
+        mPaintDoodle.setColor(mColor);
         mPaintDoodle.setAntiAlias(true);
         mPaintDoodle.setStyle(Paint.Style.STROKE);
-
+        mPaintDoodle.setStrokeWidth(mStrokeWidth);
+        mPaintDoodle.setAlpha(mOpacity);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawPath(mPath, mPaintDoodle);
-
+        for(int i = 0; i < mPaths.size(); i++) {
+            canvas.drawPath(mPaths.get(i), mPaintObjects.get(i));
+        }
     }
 
     @Override
@@ -60,10 +84,29 @@ public class DoodleView extends View {
                 mPath.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
+                mPaths.add(mPath);
+                mPaintObjects.add(mPaintDoodle);
+                mPath = new Path();
+                mPaintDoodle = new Paint();
+                setPaintProperties();
                 break;
-
         }
         invalidate();
         return true;
+    }
+
+    public void changeStrokeWidth(Float width) {
+        mStrokeWidth = width;
+        setPaintProperties();
+    }
+
+    public void changeOpactiy(int opacity) {
+        mOpacity = Math.abs(255 - opacity);
+        setPaintProperties();
+    }
+
+    public void changeColor(int color) {
+        mColor = color;
+        setPaintProperties();
     }
 }
--- app/src/main/java/course/examples/cmsc434doodler/MainActivity.java
+++ app/src/main/java/course/examples/cmsc434doodler/MainActivity.java
@@ -1,16 +1,116 @@
 package course.examples.cmsc434doodler;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Doodler";
+
+    private DoodleView mDoodleView;
+    private SlidingUpPanelLayout mLayout;
+    private SeekBar mOpacitySeekBar;
+    private SeekBar mPBsizeSeekBar;
+    private ImageButton redButton;
+    private ImageButton greenButton;
+    private ImageButton blueButton;
+    private ImageView slidingPanelArrow;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        mLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                Log.i(TAG, "onPanelSlide, offset " + slideOffset);
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+                Log.i(TAG, "onPanelStateChanged " + newState);
+                if (newState == SlidingUpPanelLayout.PanelState.EXPANDED) {
+                    slidingPanelArrow.setImageResource(R.drawable.ic_keyboard_arrow_down_white_24dp);
+                } else {
+                    slidingPanelArrow.setImageResource(R.drawable.ic_keyboard_arrow_up_white_24dp);
+                }
+            }
+        });
+        mDoodleView = (DoodleView) findViewById(R.id.canvas);
+        mOpacitySeekBar = (SeekBar) findViewById(R.id.seekBarOpacity);
+        mOpacitySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                progress = progressValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mDoodleView.changeOpactiy(progress);
+
+            }
+        });
+        mPBsizeSeekBar = (SeekBar) findViewById(R.id.seekBarBrushSize);
+        mPBsizeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                progress = progressValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.i(TAG, "progress:  " + progress);
+                mDoodleView.changeStrokeWidth((float) progress);
+
+            }
+        });
+        redButton = (ImageButton) findViewById(R.id.red_btn);
+        redButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeColor(Color.RED);
+            }
+        });
+        greenButton = (ImageButton) findViewById(R.id.green_btn);
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeColor(Color.GREEN);
+            }
+        });
+        blueButton = (ImageButton) findViewById(R.id.blue_btn);
+        blueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeColor(Color.BLUE);
+            }
+        });
+        slidingPanelArrow = (ImageView) findViewById(R.id.sliding_panel_arrow);
     }
 
     @Override
@@ -33,5 +133,27 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void changeColor(int color) {
+        redButton.setBackgroundResource(R.drawable.rectangle_red_v2_36);
+        greenButton.setBackgroundResource(R.drawable.rectangle_green_36);
+        blueButton.setBackgroundResource(R.drawable.rectangle_blue_36);
+        switch (color) {
+            case Color.RED:
+                redButton.setBackgroundResource(R.drawable.button_border);
+                redButton.setImageResource(R.drawable.rectangle_red_v2_36);
+                break;
+            case Color.GREEN:
+                greenButton.setBackgroundResource(R.drawable.button_border);
+                greenButton.setImageResource(R.drawable.rectangle_green_36);
+                break;
+            case Color.BLUE:
+                blueButton.setBackgroundResource(R.drawable.button_border);
+                blueButton.setImageResource(R.drawable.rectangle_blue_36);
+                break;
+
+        }
+        mDoodleView.changeColor(color);
     }
 }
